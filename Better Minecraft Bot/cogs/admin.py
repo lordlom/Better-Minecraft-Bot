8531f3447ee210662d432cca1992a65791bd6ac2
lordlom <68526730+lordlom@users.noreply.github.com>
2020-12-01 17:05:39 -0800

    Update admin.py

--- Better Minecraft Bot/cogs/admin.py
+++ Better Minecraft Bot/cogs/admin.py
@@ -59,68 +59,12 @@ class admin(commands.Cog):
 
     @commands.command()
     async def botsay(self, ctx, message: str):
-        channel = self.bot.get_channel(742122497146748988)
+        channel = self.bot.get_channel(782385370850525257)
         await channel.send(message)
 
     @commands.command()
-    async def changenick(self, ctx, member: str, newname: str):
-        await ctx.author.edit(nick=newname)
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Connected!')
-        print('Username: {0.name}\nID: {0.id}'.format(self.user))
-
-    async def on_message(self, message):
-        if message.content.startswith('!deleteme'):
-            msg = await message.channel.send('I will delete myself now...')
-            await msg.delete()
-
-            # this also works
-            await message.channel.send('Goodbye in 3 seconds...', delete_after=3.0)
-
-    async def on_message_delete(self, message):
-        fmt = '{0.author} has deleted the message: {0.content}'
-        await message.channel.send(fmt.format(message))
-
-        async def on_message(self, message):
-            if message.author.id == self.user.id:
-                return
-
-        if message.content.startswith('!hello'):
-            await message.channel.send('Hello {0.author.mention}'.format(message))
-
-async def ex(args, message, client, invoke):
-
-    try:
-        ammount = int(args[0]) + 1 if len(args) > 0 else 2
-    except:
-        await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), descrition="Please enter a valid value for message ammount!"))
-        return
-
-    cleared = 0
-    failed = 0
-
-    async for m in client.logs_from(message.channel, limit=ammount):
-        try:
-            await client.delete_message(m)
-            cleared += 1
-        except:
-            failed += 1
-            pass
-
-    failed_str = "\n\nFailed to clear %s message(s)." % failed if failed > 0 else ""
-    returnmsg = await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.blue(), description="Cleared %s message(s).%s" % (cleared, failed_str)))
-    await asyncio.sleep(4)
-    await client.delete_message(returnmsg)
-
-    @client.command()
-
-    async def kick(ctx, member: discord.Member, *, reason=None):
-
-        await member.kick(reason=reason)
-
-        await ctx.send(f'User {member} has been kicked.')
+    async def github(self, ctx):
+        await ctx.send('https://github.com/lordlom/Better-Minecraft-Bot')
 
 def setup(bot):
     bot.add_cog(admin(bot))
